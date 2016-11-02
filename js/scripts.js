0c2ef2ca595bf9b51e1e80315624623c8fc6ec99
Caitlin Ashtari <caitlinashtari@Unknown.domain>
2016-11-02 13:18:13 -0700

    add backend js with constructor function for Pizza, prototypes for delivery, size, and toppings cost, as well as add frontend to display values from newPizza in html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,76 @@
 /* backend -----------------*/
+var deliveryMethod;
+var size;
+var newPizza;
+
+function Pizza (){
+  this.deliveryMethod = deliveryMethod;
+  this.size = size;
+  this.toppings = [];
+  this.cost = 10;
+}
 
 
+Pizza.prototype.deliveryCost = function () {
+  if(this.deliveryMethod === "delivery"){
+    this.cost += 5;
+  }
+}
 
+Pizza.prototype.sizeCost = function () {
+  if(this.size === "xl"){
+    this.cost += 3
+  } else if(this.size === "large"){
+    this.cost += 2;
+  } else if(this.size === "medium"){
+    this.cost += 1;
+  }
+}
 
-
-
-
-
-
-
+Pizza.prototype.toppingsCost = function () {
+  var totalToppings = this.toppings.length;
+  this.cost += totalToppings;
+}
 
 
 /* frontend -----------------*/
 $(document).ready(function(){
   $("form#toppingsSize").submit(function(event){
     event.preventDefault();
-    $("#orderSummary").show();
 
-    $("input:checkbox[name=deliveryMethod]:checked").each(function(){
-      var deliveryMethod = $(this).val();
-      $("span#deliverySummary").append(deliveryMethod + " ");
+    newPizza = new Pizza();
+
+
+
+    $("input:checkbox[name=deliveryMethodInput]:checked").each(function(){
+      deliveryMethod = $(this).val();
+      newPizza.deliveryMethod = deliveryMethod;
     });
 
-    $("input:checkbox[name=size]:checked").each(function(){
-      var pizzaSize = $(this).val();
-      $("span#sizeSummary").append(pizzaSize + "");
+    $("input:checkbox[name=sizeInput]:checked").each(function(){
+      size = $(this).val();
+      newPizza.size = size;
     });
 
-    $("input:checkbox[name=topping]:checked").each(function(){
+    $("input:checkbox[name=toppingInput]:checked").each(function(){
       var allToppings = $(this).val();
-      $("span#toppingSummary").append(allToppings + ", ");
+      newPizza.toppings.push(allToppings);
     });
+    newPizza.deliveryCost();
+    newPizza.sizeCost();
+    newPizza.toppingsCost();
+    $("form#toppingsSize").fadeOut(500);
+    $("#orderSummary").fadeIn(2500);
+    $("#checkoutButton").fadeIn(2500);
+
+
+    $("#orderSummary").prepend("Your " + size + " pizza will hot and ready for " + deliveryMethod + " in 15 minutes, complete with:");
+
+    for(var i = 0; i <= (newPizza.toppings.length - 1); i++){
+      $("#toppingsList").append("<li>" + newPizza.toppings[i] + "</li>");
+    }
+
+    $("#orderSummary").append("<h2>Your total comes out to: $" + newPizza.cost);
+
   });
 });
